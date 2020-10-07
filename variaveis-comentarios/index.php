f70b5038de9fce933af32cd9ad064039486c39cf
Mauricio de Assis <mauriciox8mm@hotmail.com>
2020-10-07 19:02:11 -0300

    Variáveis e Comentários

---
+++ variaveis-comentarios/index.php
@@ -0,0 +1,16 @@
+<?php
+//PHP é fracamente tipado então os tipos automaticamente a linguagem intrepreta  conforme a necessidade 
+// as formas utilizadas para criar variaveis é atráves do $ _ ou ex: $nome_completo ou $nomeCompleto 
+//String pode ser declarada com apostrofo ' ' ou aspas " "
+//string 
+$nome = "Nome";
+//char
+$sexo = 'M';
+//int
+$idade = 23;
+//float 
+$salario = 2500.40;
+//booleam 
+$ativo = true;
+//GET TYPE imprime o tipo da variavel string float etc
+echo gettype($nome);
