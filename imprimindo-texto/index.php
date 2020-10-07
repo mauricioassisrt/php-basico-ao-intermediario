5b20c0db4aeb225ae7532d9930dd25fccb7b29e9
Mauricio de Assis <mauriciox8mm@hotmail.com>
2020-10-07 18:42:35 -0300

    9. Imprimindo Texto

---
+++ imprimindo-texto/index.php
@@ -0,0 +1,12 @@
+<?php
+$nome = 'Mauricio';
+//imprimindo 
+//echo "ola mundo {$nome}";
+//echo "ola mundo " . $nome;
+
+//print retorna 1 sempre, echo não retorna somente uma string 
+//print $nome;
+
+$retorno =  print $nome;
+
+echo $retorno;
