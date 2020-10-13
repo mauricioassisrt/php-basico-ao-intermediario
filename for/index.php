59421a5b34e88dc86ec8d106984b71c735771258
Mauricio de Assis <mauriciox8mm@hotmail.com>
2020-10-12 22:26:44 -0300

    for

---
+++ for/index.php
@@ -0,0 +1,35 @@
+<?php
+// for ($i = 0; $i < 10; $i++) {
+//     echo $i . '<br/>';
+// }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For</title>
+</head>
+
+<body>
+    <ul>
+        <?php
+        for ($i = 0; $i <= 10; $i++) {
+            for ($j = 0; $j <= 10; $j++) {
+        ?>
+        <li>
+            <?= $i ?>* <?= $j; ?> = <?= ($i * $j); ?>
+        </li>
+        <?php
+            }
+            echo '<br/>';
+        }
+        ?>
+
+    </ul>
+
+</body>
+
+</html>
